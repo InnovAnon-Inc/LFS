81214e99253a2fc5e5541a29ac8e9a817f87e03b
Default User <laurence.a.maddox@gmail.com>
2017-10-07 16:11:32 -0500

    backup

--- LFS2/main.sh
+++ LFS2/main.sh
@@ -7,22 +7,5 @@ cd $D
 
 . ./cnf
 
-./LFS-2.5-Creating-a-File-System-on-the-Partition0
-trap "./LFS-2.5-Creating-a-File-System-on-the-Partition0" 0
-./LFS-2.5-Creating-a-File-System-on-the-Partition
-
-./LFS-2.7-Mounting-the-New-Partition
-./LFS-3.1-Introduction
-./LFS-4.2-Creating-the-LFS-tools-Directory
-./LFS-4.3-Adding-the-LFS-User
-./LFS-4.3-Adding-the-LFS-User1 $D/LFS-4.4-Setting-Up-the-Environment
-# unnecessary: LFS-4.4-Setting-Up-the-Environment1
-./LFS-4.3-Adding-the-LFS-User1 $D/LFS-5.3-General-Compilation-Instructions
-./LFS-4.3-Adding-the-LFS-User1 $D/LFS-5.3+
-./LFS-4.3-Adding-the-LFS-User1 $D/LFS-5.36-Stripping
-./LFS-4.3-Adding-the-LFS-User1 $D/LFS-5.37-Changing-Ownership
-# TODO backup /tools
-./LFS-6.2-Preparing-Virtual-Kernel-File-Systems0
-trap "./LFS-6.2-Preparing-Virtual-Kernel-File-Systems0" 0
-./LFS-6.2-Preparing-Virtual-Kernel-File-Systems
-./LFS-6.4-Entering-the-Chroot-Environment $D/
+./LFS-2.5-Creating-a-File-System-on-the-Partition1 \
+	./LFS-2.7+
