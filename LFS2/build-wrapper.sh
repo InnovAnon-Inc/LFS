9614e24db63eb5f87837e284aa3d922f2e3981d3
Default User <laurence.a.maddox@gmail.com>
2017-10-07 15:39:43 -0500

    backup

--- LFS2/build-wrapper.sh
+++ LFS2/build-wrapper.sh
@@ -22,7 +22,7 @@ $D/LFS-2.7-Mounting-the-New-Partition
 if [ "x$2" = xtools ] ; then
 	./LFS-4.3-Adding-the-LFS-User1 $B $*
 elif [ "x$2" = xchroot ] ; then
-	? $B $*
+	./LFS-6.4-Entering-the-Chroot-Environment $B $*
 else exit 1
 fi
 
--- LFS2/main.sh
+++ LFS2/main.sh
@@ -18,8 +18,11 @@ trap "./LFS-2.5-Creating-a-File-System-on-the-Partition0" 0
 ./LFS-4.3-Adding-the-LFS-User1 $D/LFS-4.4-Setting-Up-the-Environment
 # unnecessary: LFS-4.4-Setting-Up-the-Environment1
 ./LFS-4.3-Adding-the-LFS-User1 $D/LFS-5.3-General-Compilation-Instructions
-./LFS-4.3-Adding-the-LFS-User1 $D/LFS-5.3+.sh
-./LFS-4.3-Adding-the-LFS-User1 $D/LFS-5.36-Stripping.sh
-./LFS-4.3-Adding-the-LFS-User1 $D/LFS-5.37-Changing-Ownership.sh
+./LFS-4.3-Adding-the-LFS-User1 $D/LFS-5.3+
+./LFS-4.3-Adding-the-LFS-User1 $D/LFS-5.36-Stripping
+./LFS-4.3-Adding-the-LFS-User1 $D/LFS-5.37-Changing-Ownership
 # TODO backup /tools
-#./LFS-4.3-Adding-the-LFS-User1 $D/LFS-5.37-Changing-Ownership0.sh
+./LFS-6.2-Preparing-Virtual-Kernel-File-Systems0
+trap "./LFS-6.2-Preparing-Virtual-Kernel-File-Systems0" 0
+./LFS-6.2-Preparing-Virtual-Kernel-File-Systems
+./LFS-6.4-Entering-the-Chroot-Environment $D/
