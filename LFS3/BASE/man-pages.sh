2f84b7396b48a75539d73e4680f3b36ca06142be
Default User <laurence.a.maddox@gmail.com>
2017-10-09 22:37:03 -0500

    backup

--- LFS3/BASE/man-pages.sh
+++ LFS3/BASE/man-pages.sh
@@ -4,11 +4,11 @@ cd /sources
 
 #
 
-rm -rf man-pages-4.12
-tar xf man-pages-4.12.tar.xz
-pushd  man-pages-4.12
+rm -rf man-pages-4.13
+tar xf man-pages-4.13.tar.xz
+pushd  man-pages-4.13
 
 make install
 
 popd
-rm -rf man-pages-4.12
+rm -rf man-pages-4.13
