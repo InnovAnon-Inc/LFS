f5c18e25755fb117dfa8c23b9553b10221ae3307
Linux From Scratch <lfs@example.com>
2017-10-16 06:33:36 -0500

    backup

--- LFS3/BBLFS/opencl-nvidia.sh
+++ LFS3/BBLFS/opencl-nvidia.sh
@@ -2,10 +2,11 @@ set -eo nounset
 
 cd /sources
 
+# TODO use a system group number
+#grep -q gpu /etc/group || \
+#groupadd gpu
+
 #setfacl -m g:gpu:rw /dev/nvidia*
 
-# TODO add download
-
-ls
-exit 2
+./NVIDIA-Linux-x86_64-387.12.run
 
