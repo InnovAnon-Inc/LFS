898a6c9887e7595ffe2a01e9436e112d872d3af7
Default User <laurence.a.maddox@gmail.com>
2017-10-18 03:03:36 -0500

    backup

--- LFS3/BASE/grub-boot.sh
+++ LFS3/BASE/grub-boot.sh
@@ -27,7 +27,7 @@ grub-install \
 	--no-bootsector          \
 	--removable              \
         --recheck                \
-	--target=x86_64=efi      \
+	--target=x86_64-efi      \
         --debug                  \
 	/dev/sda
 
