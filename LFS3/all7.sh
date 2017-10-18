aeab975a3b8e3cd90dfdaf6ba101803dea381c62
Default User <laurence.a.maddox@gmail.com>
2017-10-17 23:30:21 -0500

    backup

--- LFS3/all7.sh
+++ LFS3/all7.sh
@@ -6,15 +6,19 @@ export LFS=/mnt/lfs
 #./preparing-virtual-kernel-file-systems.sh
 
 # this handles the case when the host has a separate /boot partition
-mount --bind /boot $LFS/boot
-trap "umount -f /boot $LFS/boot" 0
+#mount --bind /boot $LFS/boot
+#trap "umount -f /boot $LFS/boot" 0
 
+( set -eo nounset
+cat << EOF
+cd /workspace/LFS/LFS3/BASE
+./base5.sh
+EOF
+cat
+) | \
 chroot "$LFS" /usr/bin/env -i \
     HOME=/root                  \
     TERM="$TERM"                \
     PS1='\u:\w\$ '              \
     PATH=/bin:/usr/bin:/sbin:/usr/sbin \
-    /bin/bash +h << EOF
-cd /workspace/LFS/LFS3/BASE
-./base5.sh
-EOF
+    /bin/bash +h
